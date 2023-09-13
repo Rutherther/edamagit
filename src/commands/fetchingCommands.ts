dfc3077654d353b4855b0677d8058dd6f0958651
František Boháček <fandabohacek@gmail.com>
2023-09-13 13:03:55 +0200

    Add options to set upstream and remote in fetching menu

--- src/commands/fetchingCommands.ts
+++ src/commands/fetchingCommands.ts
@@ -3,6 +3,9 @@ import { MagitRepository } from '../models/magitRepository';
 import { MenuUtil, MenuState, Switch, MenuItem } from '../menu/menu';
 import { gitRun } from '../utils/gitRawRunner';
 import { PickMenuItem, PickMenuUtil } from '../menu/pickMenu';
+import { RefType } from '../typings/git';
+import GitUtils from '../utils/gitUtils';
+import GitTextUtils from '../utils/gitTextUtils';
 
 export async function fetching(repository: MagitRepository): Promise<any> {
 
@@ -10,12 +13,16 @@ export async function fetching(repository: MagitRepository): Promise<any> {
 
   if (repository.HEAD?.pushRemote) {
     const pushRemote = repository.HEAD?.pushRemote;
-    fetchingMenuItems.push({ label: 'p', description: pushRemote.remote, action: fetchFromPushRemote });
+    fetchingMenuItems.push({ label: 'p', description: `${pushRemote.remote}/${pushRemote.name}`, action: fetchFromPushRemote });
+  } else {
+    fetchingMenuItems.push({ label: 'p', description: `pushRemote, after setting that`, action: fetchSetPushRemote });
   }
 
   if (repository.HEAD?.upstream) {
     const upstream = repository.HEAD?.upstream;
-    fetchingMenuItems.push({ label: 'u', description: upstream.remote, action: fetchFromUpstream });
+    fetchingMenuItems.push({ label: 'u', description: `${upstream.remote}/${upstream.name}`, action: fetchFromUpstream });
+  } else {
+    fetchingMenuItems.push({ label: 'u', description: `@{upstream}, after setting that`, action: fetchSetUpstream });
   }
 
   fetchingMenuItems.push({ label: 'e', description: 'elsewhere', action: fetchFromElsewhere });
@@ -33,6 +40,63 @@ export async function fetching(repository: MagitRepository): Promise<any> {
   ];
 
   return MenuUtil.showMenu({ title: 'Fetching', commands: fetchingMenuItems }, { repository, switches });
+}
+
+async function fetchSetUpstream({ repository, ...rest }: MenuState) {
+
+  let choices = [...repository.refs];
+
+  if (repository.remotes.length > 0 &&
+    !choices.find(ref => ref.name === repository.remotes[0].name + '/' + repository.HEAD?.name)) {
+    choices = [{
+      name: `${repository.remotes[0].name}/${repository.HEAD?.name}`,
+      remote: repository.remotes[0].name,
+      type: RefType.RemoteHead
+    }, ...choices];
+  }
+
+  const refs: PickMenuItem<string>[] = choices
+    .filter(ref => ref.type !== RefType.Tag && ref.name !== repository.HEAD?.name)
+    .sort((refA, refB) => refB.type - refA.type)
+    .map(r => ({ label: r.name!, description: GitTextUtils.shortHash(r.commit), meta: r.name! }));
+
+  let chosenRemote;
+  try {
+    chosenRemote = await PickMenuUtil.showMenu(refs);
+  } catch { }
+
+  const ref = repository.HEAD?.name;
+
+  if (chosenRemote && ref) {
+
+    const [remote, name] = GitTextUtils.remoteBranchFullNameToSegments(chosenRemote);
+
+    if (remote && name) {
+
+      await GitUtils.setConfigVariable(repository, `branch.${ref}.merge`, `refs/heads/${name}`);
+      await GitUtils.setConfigVariable(repository, `branch.${ref}.remote`, remote);
+
+      repository.HEAD!.upstreamRemote = { name, remote };
+
+      return fetchFromUpstream({ repository, ...rest });
+    }
+  }
+}
+
+async function fetchSetPushRemote({ repository, ...rest }: MenuState) {
+  const remotes: PickMenuItem<string>[] = repository.remotes
+    .map(r => ({ label: r.name, description: r.pushUrl, meta: r.name }));
+
+  const chosenRemote = await PickMenuUtil.showMenu(remotes);
+
+  const ref = repository.HEAD?.name;
+
+  if (chosenRemote && ref) {
+    await GitUtils.setConfigVariable(repository, `branch.${ref}.pushRemote`, chosenRemote);
+
+    repository.HEAD!.pushRemote = { name: ref, remote: chosenRemote };
+    return fetchFromPushRemote({ repository, ...rest });
+  }
 }
 
 async function fetchFromPushRemote({ repository, switches }: MenuState) {
